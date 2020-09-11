c24e047b9e4b2983ed7805aeef99872111af3024
Waflia <andrey.vaniukov@yandex.ru>
2020-09-11 15:26:14 +0300

    Added data class Models for saving data about each item

--- app/src/main/java/com/waflia/keddit/MainActivity.kt
+++ app/src/main/java/com/waflia/keddit/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         }
         ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out,
             R.anim.abc_popup_enter, R.anim.abc_popup_exit)
-        //ft.replace(R.id.base_content, f)
+        ft.replace(R.id.activity_base_content, f)
         ft.addToBackStack(null)
         ft.commit()
     }
---
+++ app/src/main/java/commons/Models.kt
@@ -0,0 +1,10 @@
+package commons
+
+data class RedditNewsItem(
+        val author: String,
+        val title: String,
+        val numComments: Int,
+        val created: Long,
+        val thumbnail: String,
+        val url:String
+)
